514e497650890473a759d8fa2d369680c9062d7b
slavaider <slavaider1@gmail.com>
2021-02-10 19:29:38 +0500

    fetch: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array ==='undefined' || (Array.isArray(array) &&array.length===0)) {
+        return 0
+    }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array ==='undefined' || (Array.isArray(array) &&array.length===0)) {
+        return 0
+    }
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(typeof array ==='undefined' || (Array.isArray(array) &&array.length===0)) {
+        return 0
+    }
+    return array.reduce((total,el)=>total+el,0)/array.length;
 }
